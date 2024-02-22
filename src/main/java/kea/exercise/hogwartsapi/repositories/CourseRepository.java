59246d9470d04469c95bea7d539a14acb7f9fd8e
Jaes98 <marcus.jaes@gmail.com>
2024-02-22 15:14:10 +0100

    repositories oprettet

--- src/main/java/kea/exercise/hogwartsapi/repositories/CourseRepository.java
+++ src/main/java/kea/exercise/hogwartsapi/repositories/CourseRepository.java
@@ -1,4 +1,9 @@
 package kea.exercise.hogwartsapi.repositories;
 
-public class CourseRepository {
+import kea.exercise.hogwartsapi.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Integer>{
 }
--- src/main/java/kea/exercise/hogwartsapi/repositories/HouseRepository.java
+++ src/main/java/kea/exercise/hogwartsapi/repositories/HouseRepository.java
@@ -1,4 +1,9 @@
 package kea.exercise.hogwartsapi.repositories;
 
-public class HouseRepository {
+import kea.exercise.hogwartsapi.models.House;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HouseRepository extends JpaRepository<House, Integer> {
 }
