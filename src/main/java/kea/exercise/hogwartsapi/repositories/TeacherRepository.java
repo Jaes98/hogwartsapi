8c4eceb1e26076edebdeabfa22c7beb08ba3b929
Jaes98 <marcus.jaes@gmail.com>
2024-02-22 15:11:01 +0100

    inital commits og startup på models

--- src/main/java/kea/exercise/hogwartsapi/repositories/TeacherRepository.java
+++ src/main/java/kea/exercise/hogwartsapi/repositories/TeacherRepository.java
@@ -1,4 +1,9 @@
 package kea.exercise.hogwartsapi.repositories;
 
-public class TeacherRepository {
+import kea.exercise.hogwartsapi.models.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Integer>{
 }
